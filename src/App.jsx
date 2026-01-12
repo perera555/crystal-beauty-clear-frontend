98d4f02b0507ae42856da4d391af861789b88043
Dhanushka <pthamara@rocketmail.com>
2026-01-12 17:01:51 +0530

    day 11 ✏️

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import ProductCard from './components/productCard'
+
+
+function App() {
+
+
+  return (
+    <>
+
+
+        <ProductCard name="Apple Ipad" price="$999" image="https://hips.hearstapps.com/hmg-prod/images/apple-ipad-mini-2024-review-lead-672a0d53e55b6.jpg?crop=0.6666666666666666xw:1xh;center,top&resize=1200:*" />
+        <ProductCard name="macbook Pro" price="$1999" image="https://media.istockphoto.com/id/1425655509/photo/apple-macbook-pro.jpg?s=612x612&w=0&k=20&c=Qvvkux4tUjgOnZInhR6C8zQdpnx_YHS4RhKBHZ3-ouM=" />
+          
+          
+   
+   
+   
+    
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,15 @@
+import"./productCard.css"
+export default function ProductCard(props) {
+
+    return (
+        <div className="productCard">
+            <h1>{props.name}</h1>
+            <p>{props.price}</p>
+            <img className="productImage" src={props.image} alt="" />
+            <button>Add to Cart</button>
+
+        </div>
+
+    )
+
+}
